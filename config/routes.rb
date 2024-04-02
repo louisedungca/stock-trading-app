055a474c0276dd9ee338e25c990ccdf5a6303012
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-02 20:34:57 +0800

    chore: add dashboard controllers for admin and trader

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
+  namespace :trader, path: 't' do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
   # routes for static pages
   get 'landing', to: 'pages#landing', as: :landing_page
 end
