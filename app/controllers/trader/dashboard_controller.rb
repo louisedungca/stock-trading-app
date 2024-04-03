67480afa1289c51eb60f3631b1710d770d623c91
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-03 14:34:35 +0800

    fix: authorize login for approved traders

--- app/controllers/trader/dashboard_controller.rb
+++ app/controllers/trader/dashboard_controller.rb
@@ -10,8 +10,6 @@ class Trader::DashboardController < ApplicationController
     if current_user.trader? && current_user.status&.status_type == "pending"
       sign_out(current_user)
       redirect_to pending_approval_page_path
-    else
-      redirect_to trader_dashboard_path
     end
   end
 
