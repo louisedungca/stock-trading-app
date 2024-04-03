7bfcbe2780f248d09ccc79290a98dc59b65f40e1
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-03 17:03:21 +0800

    fix: add username as login option aside from email

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,10 @@
 #  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
+  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :uid
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :uid, authentication_keys: [:login]
 
   has_one :status
 
@@ -39,6 +40,20 @@ class User < ApplicationRecord
   before_create :initialize_status_for_trader
 
   scope :approved_traders, -> { includes(:status).where(role: :trader, statuses: { status_type: "approved" }) }
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+
+    if(login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   private
 
