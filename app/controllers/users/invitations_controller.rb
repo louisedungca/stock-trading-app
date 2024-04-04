cd8f773f1b4979da1ae5591a1621c9910925c026
cyrilcero <cyril.cero@gmail.com>
2024-04-05 01:44:03 +0800

    feat: add redirect to admin dashboard after sending an invite

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -3,9 +3,20 @@
 class Users::InvitationsController < Devise::InvitationsController
   before_action :authenticate_admin!
 
+  def after_invite_path_for(inviter, invitee)
+    admin_dashboard_path
+    #insert alert notice here
+  end
+
   private
 
   def authenticate_admin!
     redirect_to trader_dashboard_path, alert: "You are not authorized to perform this action." unless current_user.admin?
+  end
+
+  def invite_params # GPT
+    params.require(:user).permit(:email, :role).tap do |whitelisted|
+      whitelisted[:username] = params[:user][:email].split('@').first if params[:user][:email].present?
+    end
   end
 end
