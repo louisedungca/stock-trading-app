cffc41eccc7c8802d45ad80c6a610effb844755c
cyrilcero <cyril.cero@gmail.com>
2024-04-04 20:21:17 +0800

    fix: add guard clause for devise confirmable

--- app/controllers/trader/dashboard_controller.rb
+++ app/controllers/trader/dashboard_controller.rb
@@ -8,10 +8,9 @@ class Trader::DashboardController < ApplicationController
   private
 
   def check_status
-    if current_user.trader? && current_user.status&.status_type == "pending"
+    if current_user.trader? && current_user.status&.status_type == "pending" && !current_user.confirmed?
       sign_out(current_user)
       redirect_to pending_verification_page_path
     end
   end
-
 end
