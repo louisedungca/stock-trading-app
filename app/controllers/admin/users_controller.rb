a13b9cf50cb5a7530d97e306bf5a8f49bb0654a0
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-07 18:40:14 +0800

    feat: create edit user details modal

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,15 +2,32 @@ class Admin::UsersController < ApplicationController
   layout "dashboard_layout"
 
   def index
-    @traders = User.where(role: "trader")
+    @traders = User.where(role: "trader").order(:created_at)
   end
 
   def edit
+    @trader = User.find(params[:id])
   end
 
   def update
+    @trader = User.find(params[:id])
+    if @trader.update(trader_params)
+      redirect_to admin_users_path, notice: "User details successfully updated."
+    else
+      flash[:alert] = @trader.errors.full_messages.join(". ")
+      redirect_back(fallback_location: admin_users_path)
+    end
   end
 
   def destroy
+    @trader = User.find(params[:id])
+    @trader.destroy
+    redirect_to admin_users_path, notice: "User permanently deleted."
+  end
+
+  private
+
+  def trader_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
