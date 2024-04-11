9074e7f8255001646450e7400fd8c32cf72d5b26
cyrilcero <cyril.cero@gmail.com>
2024-04-11 16:48:13 +0800

    feat: add conditional filter for UID specific transactions

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -2,16 +2,14 @@ class Admin::TransactionsController < ApplicationController
   layout 'dashboard_layout'
 
   def index
-    @transactions = case params[:filter]
-                    when 'buy'
-                      Transaction.where(transaction_type: 'buy')
-                    when 'sell'
-                      Transaction.where(transaction_type: 'sell')
-                    when 'cash_in'
-                      Transaction.where(transaction_type: 'cash_in')
+    @transactions = if params[:search].present?
+                      Transaction.joins(:user)
+                                 .where(users: { uid: params[:search] })
+                                 .filter_by_type(params[:filter])
                     else
-                      Transaction.all
+                      Transaction.filter_by_type(params[:filter])
                     end
+
     @pagy, @transactions = pagy_array(@transactions) || pagy(@transactions)
   end
 end
