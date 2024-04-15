e283b6e7cd2b3599754b317465c8c719bf7e9e73
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-15 09:31:00 +0800

    feat: add assets box on trader dashboard

--- app/controllers/trader/dashboard_controller.rb
+++ app/controllers/trader/dashboard_controller.rb
@@ -2,6 +2,16 @@ class Trader::DashboardController < TradersController
   layout 'dashboard_layout'
 
   def index
+    @stocks = current_user.stocks.order(updated_at: :desc)
+    @total_portfolio_value = 0.0
+
+    # Fetch quote for each stock
+    @stock_quotes = {}
+    @stocks.each do |stock|
+      @stock_quotes[stock.stock_symbol] = IEX::Api::Client.new.quote(stock.stock_symbol)
+      total_price = @stock_quotes[stock.stock_symbol].latest_price * stock.shares
+      @total_portfolio_value += total_price
+    end
   end
 
   def market
