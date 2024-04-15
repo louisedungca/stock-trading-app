cf25c9f36726938352ea023f9163b9b7982a1f8f
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-15 08:43:03 +0800

    feat: add total portfolio value

--- app/controllers/trader/portfolios_controller.rb
+++ app/controllers/trader/portfolios_controller.rb
@@ -9,12 +9,15 @@ class Trader::PortfoliosController < TradersController
                 current_user.stocks.order(shares: :desc)
               end
 
-    @pagy, @stocks = pagy_array(@stocks) || pagy(@stocks)
+    @pagy, @stocks = pagy_array(@stocks) || pagy(@stocks) ## pagy stuff
+    @total_portfolio_value = 0.0
 
     # Fetch quote for each stock
     @stock_quotes = {}
     @stocks.each do |stock|
       @stock_quotes[stock.stock_symbol] = IEX::Api::Client.new.quote(stock.stock_symbol)
+      total_price = @stock_quotes[stock.stock_symbol].latest_price * stock.shares
+      @total_portfolio_value += total_price
     end
   end
 end
