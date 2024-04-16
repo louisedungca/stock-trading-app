8b5ad436c8ac440d36107546f697f7a9fb305e8d
cyrilcero <cyril.cero@gmail.com>
2024-04-16 15:29:38 +0800

    fix: change total shares logic and add validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -13,11 +13,13 @@ class Stock < ApplicationRecord
   has_many :transactions
   has_many :users, through: :transactions
 
-  validates :stock_symbol, presence: true
+  validates :stock_symbol, presence: true, uniqueness: { case_sensitive: false }
   validates :company_name, presence: true
   validates :logo_url, presence: true
 
   def total_shares
-    transactions.sum(:shares)
+    buy = transactions.where(transaction_type: 'buy').sum(:shares).to_f
+    sell = transactions.where(transaction_type: 'sell').sum(:shares).to_f
+    buy - sell
   end
 end
