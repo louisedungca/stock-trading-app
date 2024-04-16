4aaac77454ca55f7a7cb172c966e3865a5c73b3f
cyrilcero <cyril.cero@gmail.com>
2024-04-16 18:04:36 +0800

    fix: update controller logic

--- app/controllers/trader/cash_in_controller.rb
+++ app/controllers/trader/cash_in_controller.rb
@@ -9,31 +9,12 @@ class Trader::CashInController < TradersController
   def update
     amount = params[:amount].to_f
 
-    if amount <= 0
-      flash[:alert] = "Invalid amount. Please enter a value greater than 0."
-      redirect_to trader_cash_in_path
-      return
-    end
-
-    current_user.balance += amount
-
-    if current_user.save
-      Transaction.create!(
-        user: current_user,
-        transaction_type: "cash_in",
-        amount:
-      )
+    if Transaction.cash_in(current_user, amount)
       flash[:notice] = "Successfully cashed in #{format_currency(amount, unit: "$")}."
       redirect_to trader_dashboard_path
     else
       flash[:alert] = "Failed to cash in. Please try again."
       redirect_to trader_cash_in_path
     end
-  end
-
-  private
-
-  def cash_in_params
-    params.require(:user).permit(:amount)
   end
 end
