93a83e134e8948ece487936566c946547bbedf8a
cyrilcero <cyril.cero@gmail.com>
2024-04-16 20:42:48 +0800

    fix: add validations to user balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,7 @@ class User < ApplicationRecord
   }, default: 'trader'
 
   validates :email, presence: true, uniqueness: true
+  validates :balance, numericality: { greater_than_or_equal: 0, message: "Insufficient Balance." }
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
   before_create :initialize_status_for_trader
