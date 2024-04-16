37356117947c6a687c28bf369378e1324973a8c7
cyrilcero <cyril.cero@gmail.com>
2024-04-16 15:27:57 +0800

    fix: change pointer to total_shares

--- app/controllers/trader/dashboard_controller.rb
+++ app/controllers/trader/dashboard_controller.rb
@@ -9,7 +9,7 @@ class Trader::DashboardController < TradersController
     @stock_quotes = {}
     @stocks.each do |stock|
       @stock_quotes[stock.stock_symbol] = IEX::Api::Client.new.quote(stock.stock_symbol)
-      total_price = @stock_quotes[stock.stock_symbol].latest_price * stock.shares
+      total_price = @stock_quotes[stock.stock_symbol].latest_price * stock.total_shares
       @total_portfolio_value += total_price
     end
   end
