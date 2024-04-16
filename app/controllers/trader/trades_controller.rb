4fbc34a3d3be3b2d72262661a3447832b8b5389c
cyrilcero <cyril.cero@gmail.com>
2024-04-16 18:06:39 +0800

    fix: update logic for buy and sell methods

--- app/controllers/trader/trades_controller.rb
+++ app/controllers/trader/trades_controller.rb
@@ -1,4 +1,5 @@
 class Trader::TradesController < TradersController
+  before_action :set_params, except: :index
   layout 'dashboard_layout'
 
   def index
@@ -13,15 +14,27 @@ class Trader::TradesController < TradersController
   end
 
   def buy
-    stock_symbol = params[:stock_symbol]
-    shares = params[:shares].to_f
-
-    if Transaction.buy_shares(current_user, stock_symbol, shares)
-      flash[:notice] = "#{stock_symbol} stock purchased successfully"
+    if Transaction.buy_shares(current_user, @stock_symbol, @shares)
+      flash[:notice] = "#{@stock_symbol} stock purchased successfully"
     else
-      flash[:alert] = 'Error'
+      flash[:alert] = 'Error buying shares'
     end
     redirect_back(fallback_location: trader_trade_path)
   end
 
+  def sell
+    if Transaction.sell_shares(current_user, @stock_symbol, @shares)
+      flash[:notice] = "#{@stock_symbol} stock sold successfully"
+    else
+      flash[:alert] = 'Error selling shares'
+    end
+    redirect_back(fallback_location: trader_trade_path)
+  end
+
+  private
+
+  def set_params
+    @stock_symbol = params[:stock_symbol]
+    @shares = params[:shares].to_f
+  end
 end
