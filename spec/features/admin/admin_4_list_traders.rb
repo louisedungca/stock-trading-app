a1d73ab42c9954a4b5a9516a0235707220431186
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-19 11:33:20 +0800

    refactor: added scope for inspecting

--- spec/features/admin/admin_4_list_traders.rb
+++ spec/features/admin/admin_4_list_traders.rb
@@ -1,19 +1,21 @@
-# $ bundle exec rspec spec/features/invite_a_trader_spec.rb
+# $ bundle exec rspec spec/features/admin/admin_4_list_traders.rb
 require 'rails_helper'
 
 RSpec.describe 'List all traders', type: :feature do
   scenario 'Admin views all the traders' do
-    admin = create(:user, :admin) # create admin (using FactoryBot methods)
+    admin = create(:user, :admin)
     trader1 = create(:user, :trader)
     trader2 = create(:user, :trader)
     trader3 = create(:user, :trader)
-    sign_in admin # sign in as admin (using Devise Test Integration helper)
 
-    visit root_path
+    sign_in admin
+
     visit admin_users_path
 
-    expect(page).to have_content(trader1.username)
-    expect(page).to have_content(trader2.username)
-    expect(page).to have_content(trader3.username)
+    within("tbody") do
+      expect(page).to have_content(trader1.username)
+      expect(page).to have_content(trader2.username)
+      expect(page).to have_content(trader3.username)
+    end
   end
 end
