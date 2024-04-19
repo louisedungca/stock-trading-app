0311b696a0a4966485edac30aa990a812713afca
cyrilcero <cyril.cero@gmail.com>
2024-04-19 19:49:12 +0800

    fix: comment out action mailer default url options

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "rails/test_unit/railtie"
-
-# require 'factory_bot_rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
