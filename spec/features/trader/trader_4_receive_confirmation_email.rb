faf0152429a0c37485998ce858071a2ce508b1c5
Louise Dungca <dungca.louisechristine@gmail.com>
2024-04-19 15:44:11 +0800

    refactor: login trader again after approval for checking of status

--- spec/features/trader/trader_4_receive_confirmation_email.rb
+++ spec/features/trader/trader_4_receive_confirmation_email.rb
@@ -1,14 +1,15 @@
-# $ bundle exec rspec spec/features/invite_a_trader_spec.rb
+# $ bundle exec rspec spec/features/trader/trader_4_receive_confirmation_email.rb
 require 'rails_helper'
 
 RSpec.describe 'Receive approval', type: :feature do
   scenario 'Trader receives app approval' do
+    admin = create(:user, :admin)
     trader = {
       username: 'testing_account',
       email: 'testing_account@email.com',
       password: 'asdfasdf'
     }
-    admin = create(:user, :admin)
+
     # Navigate to signup
     visit root_path
     click_on 'Get Started'
@@ -16,7 +17,9 @@ RSpec.describe 'Receive approval', type: :feature do
     fill_in 'Email', with: trader[:email]
     fill_in 'Password', with: trader[:password]
     fill_in 'Password confirmation', with: trader[:password]
+
     click_on 'Register'
+
     expect(page).to have_content('Verify Your Email')
     expect(page).to have_content('Check your email and click the link to activate your account.')
 
@@ -25,6 +28,7 @@ RSpec.describe 'Receive approval', type: :feature do
     email = ActionMailer::Base.deliveries.last
     expect(email.to).to eq([trader[:email]])
     expect(email.subject).to eq('Confirmation instructions')
+
     # Click the verification link
     user = User.find_by(email: trader[:email])
     visit user_confirmation_path(confirmation_token: user.confirmation_token)
@@ -36,20 +40,48 @@ RSpec.describe 'Receive approval', type: :feature do
     fill_in 'user[password]', with: trader[:password]
     click_on 'Login'
 
+    within(".profile-box") do
+      expect(page).to have_content(trader[:username])
+      expect(page).to have_content("Pending Approval")
+    end
+
     expect(page).to have_content("We're currently evaluating your account.")
     expect(page).to have_content('Kindly wait for the confirmation email regarding the status of your account.')
     expect(page).to have_content('You can browse through the market page in the meantime.')
-    visit destroy_user_registration_path
+
+    visit destroy_user_session_path
 
     # Admin Side Approval
     sign_in admin
-    visit admin_root_path
-    click_on 'Approve'
+    visit admin_dashboard_path
+    within(".header") do
+      expect(page).to have_text("Admin")
+    end
+
+    within(".pending-approvals") do # using scope to make sure that the trader is under pending box
+      expect(page).to have_content(trader[:email])
+
+      approve_button = find(".approve-btn", match: :first)
+      approve_button.click
+    end
 
     # Check if email is sent/received
     expect(ActionMailer::Base.deliveries.count).to eq(2)
     email = ActionMailer::Base.deliveries.last
     expect(email.to).to eq([trader[:email]])
     expect(email.subject).to eq("You're Approved! Let's Start Trading with Stocker!")
+
+    sign_out admin
+    visit new_user_session_path
+    click_on 'Login'
+    fill_in 'user[login]', with: trader[:email]
+    fill_in 'user[password]', with: trader[:password]
+    click_on 'Login'
+
+    within(".profile-box") do
+      expect(page).to have_content(trader[:username])
+      expect(page).to have_content("All-access")
+    end
+
   end
 end
